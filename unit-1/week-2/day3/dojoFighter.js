0fb1e99f2efca74eb6957b4f864c04f781dbebf9
Brandon Haupt <haupt.brandon@gmail.com>
2022-10-01 18:11:45 -0500

    got attack working

--- unit-1/week-2/day3/dojoFighter.js
+++ unit-1/week-2/day3/dojoFighter.js
@@ -1,16 +1,5 @@
-const fighter = {
-    name: "Frank",
-    health: 100,
-    strength: 10,
-    defense: 10,
-}
-
-const defender = {
-    name: "Steve",
-    health: 100,
-    strength: 10,
-    defense: 10,
-}
+let fighterHealth = 10
+let defenderHealth = 10
 
 class Fighter {
     constructor(name, health, strength, defense){
@@ -19,15 +8,41 @@ class Fighter {
         this.strength = strength
         this.defense = defense
     }
-
     // Encapsulation 
-    attack(){
-        let damage = Math.floor((Math.random * fighter.strength) - defender.defense)
-        console.log(`Attacker did ${damage} to ${defender}, ${defender} now has ${this.health}`)
+    // Step 1
+    attackDefender(){
+        // grabbing a random number based on the fighters strength adding the fighters strength, then minus the defenders defense to get our total daman
+        let damage = Math.floor(((Math.random() * fighter.strength) + fighter.strength) - defender.defense)
+        // getting the health after the damage
+        let health = fighterHealth - damage
+        // console
+        console.log(`Attacker ${fighter.name} did ${damage} to defender ${defender.name}, ${defender.name} now has ${health} health`)
+        // console.log(damage)
+    }
+
+    attackFighter(){
+        // grabbing a random number based on the fighters strength adding the fighters strength, then minus the defenders defense to get our total daman
+        let damage = Math.floor(((Math.random() * defender.strength) + defender.strength) - fighter.defense)
+        // getting the health after the damage
+        let health = defenderHealth - damage
+        
+        defenderHealth -= damage
+        
+        // console
+        console.log(`Attacker ${defender.name} did ${damage} to fighter ${fighter.name}, ${fighter.name} now has ${health} health`)
+        // console.log(damage)
     }
 }
 
+const fighter = new Fighter("Frank", 10, 5, 2)
+const defender = new Fighter("Defender", 10, 5, 2)
 
-while (fighter.health >= 0) {
-    Fighter()
-}
+console.log(`Current health: ${defenderHealth}`)
+// while (fighterHealth >= 0 || defenderHealth >= 0) {
+//     fighter.attackDefender()
+//     defender.attackFighter()
+// }
+
+// console.log(fighter.attack())
+fighter.attackDefender()
+defender.attackFighter()
