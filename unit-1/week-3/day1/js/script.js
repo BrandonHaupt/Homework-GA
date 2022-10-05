ef3387bfe439719db8d0a9c94486d7c847715475
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-04 19:09:26 -0500

    updated script.js

--- unit-1/week-3/day1/js/script.js
+++ unit-1/week-3/day1/js/script.js
@@ -3,6 +3,16 @@ let titleEl = document.getElementById(`title`)
 let pEl = document.querySelector(".cool")
 let p = document.querySelector('p')
 
+// Selecting the class of Green
+let p2 = document.querySelector('.green')
+
+
+// Edit the text of a node
+p.innerHTML = 'I am the new text'
+p2.style.color = 'green'
+p2.style.textAlign = 'center'
+
+// Console logs
 console.log(p)
 console.log(pEl)
 titleEl.style.textAlign = `center`
