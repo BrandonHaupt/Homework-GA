afbd8c49bd4aeef845b64155513e83aee8cb4db2
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-04 19:27:38 -0500

    added css file structure

--- unit-1/week-3/day1/js/script.js
+++ unit-1/week-3/day1/js/script.js
@@ -2,6 +2,7 @@
 let titleEl = document.getElementById(`title`)
 let pEl = document.querySelector(".cool")
 let p = document.querySelector('p')
+let body = document.querySelector('body')
 
 // Selecting the class of Green
 let gh1 = document.querySelector('.green')
@@ -12,8 +13,11 @@ let rh1 = document.querySelector("#red")
 
 // Edit the text of a node
 p.innerHTML = 'I am the new text'
+
+// Editing the style
 gh1.style.color = 'green'
 rh1.style.color = "red"
+body.style.fontFamily = "arial"
 
 // Console logs
 console.log(p)
