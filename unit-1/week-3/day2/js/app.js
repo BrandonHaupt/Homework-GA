862521b2f4f5c7a0690b61831b5a4085476c4ef7
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-05 18:24:46 -0500

    Testing some click events

--- unit-1/week-3/day2/js/app.js
+++ unit-1/week-3/day2/js/app.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+const firstButton = document.querySelector(".click-events button")
+
+let counter = 0
+
+// firstButton.onclick = function(){
+//     alert(`This works!`)
+// }
+
+//add a click event - method 2
+//This way you usually do it
+function handleClick(){
+    alert("Wuba luba dub dub")
+}
+
+firstButton.addEventListener("click", handleClick)
+
+
+console.log(counter)
+console.log(firstButton)
