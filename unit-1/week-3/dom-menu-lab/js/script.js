080cba5ce112133135eb784e9b73594633be0b8e
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-05 20:45:13 -0500

    part 2 of DOM Menu Lab

--- unit-1/week-3/dom-menu-lab/js/script.js
+++ unit-1/week-3/dom-menu-lab/js/script.js
@@ -1,13 +1,29 @@
 let aLink = document.createElement("a")
 let nav = document.querySelector('nav')
+let subNav = document.querySelector('#sub-menu')
+
 
 let menuLinks = [
     {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-]
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ]
 
+//   let topMenuLinks
+
+let showingSubMenu = false
 
 for (let link of menuLinks) {
     const newA = document.createElement("a")
