87ef8b03f40d8600495f1573c5109781877485b5
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-08 10:17:49 -0500

    Learning JQuery

--- unit-1/week-3/day3/js/app.js
+++ unit-1/week-3/day3/js/app.js
@@ -1,25 +1,34 @@
 
-// JQuery way - Assign a Div to a variable
-const $div = $(`div`) // Assign a Div to a variable
-// Plain JS - Assign a DIV to a Variable
-const div = document.querySelector(`div`)
+// // JQuery way - Assign a Div to a variable
+// const $div = $(`div`) // Assign a Div to a variable
+// // Plain JS - Assign a DIV to a Variable
+// const div = document.querySelector(`div`)
 
-// JQuery way - Changing the DIVs text
-$div.html("new text")
-// Plain JS - Changing the DIVs text
-div.innerHTML = "New Text"
+// // JQuery way - Changing the DIVs text
+// $div.html("new text")
+// // Plain JS - Changing the DIVs text
+// div.innerHTML = "New Text"
 
-// JQuery way - Change the CSS Property
-$div.css("color", "green")
-// Plain JS - Changing the CSS property Plain JS
-div.style.color = "black"
+// // JQuery way - Change the CSS Property
+// $div.css("color", "green")
+// // Plain JS - Changing the CSS property Plain JS
+// div.style.color = "black"
 
-// JQuery way - Adding a CSS Class
-$div.addClass("myStyle")
-// Plain JS - Adding a CSS Class
-div.classList.add("myStyle")
+// // JQuery way - Adding a CSS Class
+// $div.addClass("myStyle")
+// // Plain JS - Adding a CSS Class
+// div.classList.add("myStyle")
 
-// JQuery way - Create a new Element
-const $h1 = $("<h1>")
-// Plain JS - Create a new element
-const h1 = document.createElement("h1")
+// // JQuery way - Create a new Element
+// const $h1 = $("<h1>")
+// // Plain JS - Create a new element
+// const h1 = document.createElement("h1")
+
+
+// assign the div to a variable with JQuery
+
+const $div = $('div')
+console.log($div)
+
+//Changing the text
+$div.text("This is some new text that is going to appear due to JQuery")
