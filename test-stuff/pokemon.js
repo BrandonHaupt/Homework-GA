d4b698eb36be78582d3366e97a335a62fa120a61
Brandon Haupt <haupt.brandon@gmail.com>
2022-10-09 21:33:01 -0500

    Updated pokemon js and json

--- test-stuff/pokemon.js
+++ test-stuff/pokemon.js
@@ -1,9 +1,11 @@
 import pokemon from './pokemon.json' assert {type: 'json'}
 
+
 let poke = []
 
 for (let p in pokemon) {
-  poke.push(pokemon[p])
-  poke.flat()
-  console.log(poke)
+    poke.push(pokemon[p])
+    console.log(poke)
 }
+
+console.log(poke.name)
