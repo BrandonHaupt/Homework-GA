9871b669dac4ddf50b465c7b2e6c9950f86e281c
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-12 19:29:59 -0500

    Created Cookie Clicker folder Structure
    - moved pokemon files into the pokemon folder

--- unit-1/week-4/day2/exercise/js/app.js
+++ unit-1/week-4/day2/exercise/js/app.js
@@ -1,7 +1,38 @@
-setTimeout(
-    () => {
-        console.log("World")
-    }, 2000
-)
+// setTimeout(
+//     () => {
+//         console.log("World")
+//     }, 2000
+// )
 
-console.log("Hello")
+// setInterval(() => {
+//     console.log("Welcome")
+// }, 2100)
+
+// const iceCreams = [
+//     'vanilla',
+//     "chocolate",
+//     "strawberry",
+//     "rocky road"
+// ]
+
+// iceCreams.forEach(currentElement =>{
+//     console.log(currentElement)
+// })
+
+const henchman = () => {
+    console.log("Right-o boss")
+}
+
+const henchman2 = () => {
+    console.log("You got it boss!")
+}
+
+const mobBoss = (employee) => {
+    console.log("Go to the restaurant on fire")
+    employee()
+    henchman2()
+}
+
+mobBoss(henchman)
+
+// console.log(mobBoss)
