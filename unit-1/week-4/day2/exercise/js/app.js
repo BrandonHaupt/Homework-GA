869cf0a0e0561720e0e8aee98a7484eaca384526
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-12 20:16:08 -0500

    Created Call Back drill folder structure

--- unit-1/week-4/day2/exercise/js/app.js
+++ unit-1/week-4/day2/exercise/js/app.js
@@ -19,20 +19,65 @@
 //     console.log(currentElement)
 // })
 
-const henchman = () => {
-    console.log("Right-o boss")
-}
+// const henchman = () => {
+//     console.log("Right-o boss")
+// }
 
-const henchman2 = () => {
-    console.log("You got it boss!")
-}
+// const henchman2 = () => {
+//     console.log("You got it boss!")
+// }
 
-const mobBoss = (employee) => {
-    console.log("Go to the restaurant on fire")
-    employee()
-    henchman2()
-}
+// const mobBoss = (employee) => {
+//     console.log("Go to the restaurant on fire")
+//     employee()
+//     henchman2()
+// }
 
-mobBoss(henchman)
+// mobBoss(henchman)
 
 // console.log(mobBoss)
+
+const random = Math.floor(Math.random() * 4)
+
+const c = () => {
+    console.log(`you dealt ${random} damage to the slime!`)
+}
+
+const adding = (a) => {
+    a()
+}
+
+adding(c)
+
+
+// function recievedACallBack(cd){
+//     console.log(cd())
+// }
+
+// function callBack1(){
+//     return 1
+// }
+
+// recievedACallBack(callBack1)
+
+// recievedACallBack(()=>{return 5})
+
+
+const iceCreams = [
+    "Vanilla",
+    "Chocolate",
+    "Rocky Road",
+    "Butter Pecan",
+    "Cookie Dough",
+    
+]
+
+// iceCreams.forEach(ice => {
+//     console.log(ice)
+// })
+
+const flavor = iceCreams.map(flavor => {
+    return `${flavor} ice cream`
+})
+
+console.log(flavor)
