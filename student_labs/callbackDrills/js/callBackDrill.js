525adbba5c6b5def5f7e43311c5c3886cbfe3eb5
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-12 21:00:00 -0500

    update callBackDrill
    - going through the lab

---
+++ student_labs/callbackDrills/js/callBackDrill.js
@@ -0,0 +1,54 @@
+// const hi = () => {
+//     console.log('Hi')
+// }
+
+// const foo = (p) => {
+//     p()
+// }
+
+// foo(hi)
+
+// const slicerDicer = () => {
+//     console.log("Slicin and Dicin")
+// }
+
+// const electricMixer = (attachment) => {
+//     console.log(`This mixer is now: ${attachment}`)
+// }
+
+// electricMixer(slicerDicer)
+
+// const foo = (param, param2) => {
+//     param(param2, 'hello');
+// }
+
+// const bar = (param, param2) => {
+//     console.log(param2);
+// }
+
+// const baz = (param) => {
+//     console.log(param.toUpperCase());
+// }
+
+// foo(baz, 'hello')
+
+
+const wordReverse = (word) => {
+    let reverse = word.split(" ").reverse(" ").join(" ")
+    console.log(reverse)
+}
+
+const toUpperCase = (word) => {
+    console.log(word.toUpperCase())
+}
+
+const repMaster = (param, param2) => {
+    param(`${param2()} proves that I am the rep MASTER!`)
+}
+
+//"blockhead a to heart your give never proves that I am the rep MASTER!"
+// repMaster("Never give your heart to a blockhead", wordReverse);
+
+//"I FINISHED THIS PRACTICE proves that I am the rep MASTER
+repMaster("I finished this practice", toUpperCase);
+
