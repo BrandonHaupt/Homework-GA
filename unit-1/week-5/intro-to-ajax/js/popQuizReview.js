cd67bde60ada9cc08adf0ddfb697a013e4420ed1
Brandon Haupt <52766161+BrandonHaupt@users.noreply.github.com>
2022-10-18 18:30:43 -0500

    practicing with promises

---
+++ unit-1/week-5/intro-to-ajax/js/popQuizReview.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5,6]
+
+// arr.forEach(ele => {
+//     console.log(ele)A
+// });
+
+//proper way to write a .map function
+// arr.map(function(item,index){return ele + 2})
+
+// arr.map((ele) => {
+//     console.log(ele + 2)
+// })
+
+//proper way to write a reduce function
+// arr.reduce(function(acc, item, index){return acc + item}, 0)
+
+//let reducedArr = arr.reduce((a,b) => {return a += b},0)
---
+++ unit-1/week-5/intro-to-ajax/js/promise.js
@@ -0,0 +1,15 @@
+const myPromise = new Promise((resolve, reject) => {
+
+    if(true){
+        resolve(1)
+    } else {
+        reject("Sorry too bab")
+    }
+})
+
+console.log("The promise: ", myPromise)
+
+myPromise.then((result) => console.log(result))
+console.log(2)
+console.log(3)
+
