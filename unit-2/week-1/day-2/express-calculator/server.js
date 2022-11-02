d6cbe574c38daf0956f8729fa0f136d2b720c34f
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-02 18:31:19 -0500

    updated server.js
    - testing stuff

--- unit-2/week-1/day-2/express-calculator/server.js
+++ unit-2/week-1/day-2/express-calculator/server.js
@@ -12,34 +12,11 @@ app.get(`/calc`, (request, response) => {
             <link rel="stylesheet" href="./css/styles.css">
         </head>
 
-        <div class="calc-row-1">
-            <button class="7">7</button>
-            <button class="8">8</button>
-            <button class="9">9</button>
-            <button class="times">X</button>
-        </div>
-        <div class="calc-row-2">
-            <button class="4">4</button>
-            <button class="5">5</button>
-            <button class="6">6</button>
-            <button class="subtract">-</button>
-        </div>
-        <div class="calc-row-3">
-            <button class="1">1</button>
-            <button class="2">2</button>
-            <button class="3">3</button>
-            <button class="add">+</button>
-        </div>
-        <div class="calc-row-4">
-            <button class="0">0</button>
-            <button class="dot">.</button>
-            <button class="backspace"><</button>
-            <button class="equals">=</button>
-        </div>
     `)
 })
 
 
-app.listen(port, ()=> {
+app.listen(port, (num1, num2)=> {
     console.log(`Listening on port: ${port}`)
+    console.log(`Your numbers are ${num1} + ${num2} = ${num1 + num2}`)
 })
