ffa79edd7da4f2820e10337a76af0a90eec37c80
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-02 17:53:52 -0500

    update server.js
    - made another route called calc

--- unit-2/week-1/day-2/express-calculator/server.js
+++ unit-2/week-1/day-2/express-calculator/server.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
     response.send('Testicles')
 })
 
+app.get(`calc`, (request, response) => {
+    response.send('Its calulator time!')
+})
+
 
 app.listen(port, ()=> {
     console.log(`Listening on port: ${port}`)
