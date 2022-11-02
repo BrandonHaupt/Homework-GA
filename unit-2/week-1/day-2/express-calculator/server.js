39e267000be150c2f6bd73067ab132b0c978215b
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-02 17:50:57 -0500

    update Server.js
    - made a port variable

--- unit-2/week-1/day-2/express-calculator/server.js
+++ unit-2/week-1/day-2/express-calculator/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/', (request, response) => {
-
+    response.send('Testicles')
 })
 
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000")
+app.listen(port, ()=> {
+    console.log(`Listening on port: ${port}`)
 })
