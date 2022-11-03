ad851b108d532da92a369ac722863f867810f7c9
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-03 17:02:15 -0500

    Magic 8 Ball express application created.
    Magic 8 Ball express application created.

--- unit-2/week-1/day-2/express-lab-2/server.js
+++ unit-2/week-1/day-2/express-lab-2/server.js
@@ -15,6 +15,16 @@ app.get(`/tip/:total/:tipPercent`, (request, response) => {
     response.send(`The tip amount is ${tipAmount}`)
 })
 
+app.get(`/magic/:question`, (request, response) => {
+    const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    const a = Math.floor(Math.random() * answers.length)
+    console.log(a)
+
+    response.send(`
+    <h1>The answer to your question is ${answers[a]}</h1>`)
+})
+
 
 
 
