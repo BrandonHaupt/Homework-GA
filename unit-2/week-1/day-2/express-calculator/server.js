c4f5a9f066c729653c11ef0bbfce62635e9086fe
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-02 21:21:44 -0500

    update

--- unit-2/week-1/day-2/express-calculator/server.js
+++ unit-2/week-1/day-2/express-calculator/server.js
@@ -6,18 +6,17 @@ app.get('/', (request, response) => {
     response.send('Listening')
 })
 
-app.get(`/:num1/:num2`, (request, response) => {
+app.get(`/calc/:num1/:num2`, (request, response) => {
     response.send(`
         <head>
             <link rel="stylesheet" href="./css/styles.css">
         </head>
 
-        <h1>Heeellllooooooo, how's it going!</h1>
+        <h1> ${request.params.num1} and ${request.params.num2}</h1>
     `)
 })
 
 
-app.listen(port, (num1, num2)=> {
+app.listen(port, ()=> {
     console.log(`Listening on port: ${port}`)
-    console.log(`Your numbers are ${num1} + ${num2} = ${num1 + num2}`)
 })
