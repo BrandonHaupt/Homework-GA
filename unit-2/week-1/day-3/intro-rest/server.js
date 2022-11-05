90440219a10b4b7b72cd5d1766eeb4dedd8fd00b
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-05 14:56:41 -0500

    updated server.js
    - testing .post
    - testing .delete

--- unit-2/week-1/day-3/intro-rest/server.js
+++ unit-2/week-1/day-3/intro-rest/server.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//This is called MIDDLEWARE, takes a request/response body and puts it into a JSON format
+app.use(express.json())
+
 const PORT = process.env.PORT
 
-const fruits = [
+let fruits = [
     {
         name: "apple",
         color: "red",
@@ -20,15 +23,31 @@ const fruits = [
         color: "yellow",
         readyToEat: true,
     },
+  
 ];
 
 app.get("/fruits/", (req, res) => {
     res.send(fruits);
 });
 
+
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
     res.send(fruits[req.params.indexOfFruitsArray]);
 });
+
+app.post(`/addfruit`, (req, res)=>{
+    //could also do fruits = fruits.push(req.body)
+    fruits = [...fruits, req.body] // add to the fruits array
+    res.send(`added`)
+})
+
+app.delete(`/delete/:ind`, (req, res) => {
+    fruits = fruits.filter((v,i) => {
+        return i != req.params.ind
+    })
+    res.send(fruits)
+})
+
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
