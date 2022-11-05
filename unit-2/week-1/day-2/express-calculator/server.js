107c2976df00973b1a536a7a45c0c7197f4c9b92
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-05 09:33:28 -0500

    Updated Express-calculator
    - hide the port number
    - parseInt some nums
    - got it-a working

--- unit-2/week-1/day-2/express-calculator/server.js
+++ unit-2/week-1/day-2/express-calculator/server.js
@@ -1,22 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+const PORT = process.env.PORT
 
 app.get('/', (request, response) => {
     response.send('Listening')
 })
 
 app.get(`/calc/:num1/:num2`, (request, response) => {
-    response.send(`
-        <head>
-            <link rel="stylesheet" href="./css/styles.css">
-        </head>
+    const num1 = parseInt(request.params.num1)
+    const num2 = parseInt(request.params.num2)
 
-        <h1> ${request.params.num1} and ${request.params.num2}</h1>
+    response.send(`
+        <h1>${num1 + num2}</h1>
     `)
 })
 
 
-app.listen(port, ()=> {
-    console.log(`Listening on port: ${port}`)
+app.listen(PORT, ()=> {
+    console.log(`Listening on port: ${PORT}`)
 })
