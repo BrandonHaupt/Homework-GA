bf0999c8a1814eee2ceeeb1bf9868500015dadda
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-05 16:46:25 -0500

    Update

--- unit-2/week-1/day-3/mvc/server.js
+++ unit-2/week-1/day-3/mvc/server.js
@@ -1,34 +1,11 @@
 require("dotenv").config();
-const express = require("express");
+const express = require("express"); // gets the express package
+const fruitController = require(`./controllers/index`)
 const app = express();
+const PORT = process.env.PORT
 
-const POST = process.env.PORT
-
-const fruits = [
-    {
-        name: "apple",
-        color: "red",
-        readyToEat: true,
-    },
-    {
-        name: "pear",
-        color: "green",
-        readyToEat: false,
-    },
-    {
-        name: "banana",
-        color: "yellow",
-        readyToEat: true,
-    },
-];
-
-app.get("/fruits/", (req, res) => {
-    res.send(fruits);
-});
-
-app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-    res.send(fruits[req.params.indexOfFruitsArray]);
-});
+app.use(fruitController);
+app.use(express.static(__dirname + '/public'))
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
