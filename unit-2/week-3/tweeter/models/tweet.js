8a23fd772850ec0e407c364926f3f6c52c5a2206
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-16 19:56:15 -0600

    update

--- unit-2/week-3/tweeter/models/tweet.js
+++ unit-2/week-3/tweeter/models/tweet.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema // creats a shorthand for the mongoose
 // Create a new Schema
 // This will define the shape of the documents in the collections
 // Resource: https://mongoosejs.com/docs/guide.html
-
 const tweetSchema = new Schema(
     {
         title: String,
@@ -16,6 +15,12 @@ const tweetSchema = new Schema(
     {timestamps: true}
 )
 
+// Creating Tweet model : We need to convert our schema into a model-- will be stored in 'tweets' collection.  Mongo does this for you automatically
+// Model's are fancy constructors compiled from Schema definitions
+// An instance of a model is called a document.
+// Models are responsible for creating and reading documents from the underlying MongoDB Database
+// Resource: https://mongoosejs.com/docs/models.html
 const Tweet = mongoose.model('Tweet', tweetSchema)
 
+// Export Tweet model so it can be used in our controllers
 module.exports = Tweet
--- unit-2/week-3/tweeter/server.js
+++ unit-2/week-3/tweeter/server.js
@@ -4,16 +4,55 @@ const app = express()
 const PORT = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
 
+// Dependencies
+const Tweet = require('./models/tweet.js')
+
+// Middleware
+// Body parser middleware: it creates req.body
+app.use(express.urlencoded({extended: false}))
+app.use(express.json()) // allows for the ability to parse JSON from POSTMAN
 
 // Middle man between database and frontend
 const mongoose = require('mongoose')
 const db = mongoose.connection
 
-//
+
 mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+
+
+// ROUTES / CONTROLLERS
+// CREATE
+app.post('/tweets', (req, res) => {
+
+    if(error){
+        res.send('Error')
+    }
+
+    res.send(createdTweet)
+})
+
+
+// INDEX ROUTE
+app.get('/tweets', (req,res)=> {
+    Tweet.find({}, (error, foundTweets)=>{
+        res.send(foundTweets)
+        //res.render('tweets_index', {tweetsL foundTweets})
+    })
+})
+
+
+
+
+// SHOW ROUTE
+app.get('/tweets/:id', (req,res) => {
+    Tweet.findById(req.params.id, (error, foundTweet) => {
+        res.send(foundTweet)
+    })
+})
+
 
 // DATABASE connection Error/Success
 // DEFINE callback functions for various events
