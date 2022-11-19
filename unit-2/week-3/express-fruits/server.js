bfb813391c3e73b32aec0eda26b05f3089055b6f
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-19 10:05:12 -0600

    update 1.2
    - added in middleware
    - added in required items for database connection
    - added a new schema

--- unit-2/week-3/express-fruits/server.js
+++ unit-2/week-3/express-fruits/server.js
@@ -7,14 +7,18 @@ const morgan = require('morgan')
 const methodOverride = require('method-override')
 const PORT = process.env.PORT // ALlows us to hide and use are port
 const mongoose = require('mongoose') // Allows us to use Mongoose
-const DATABASE_URL = process.env.DATABASE_URL // Allows us to hide and use our database
 const app = express()
 
-// Allows us to use DELETE and other methods
-app.use(methodOverride('_method'))
 
-// Allows the use for css
-app.use('/static', express.static('public'))
+/////////////////////////////////////////
+//             MIDDLEWARE              //
+/////////////////////////////////////////
+app.use(morgan('tiny')) // Logging errors and stuff
+app.use(methodOverride('_method')) // Allows us to use DELETE and other methods
+app.use('/static', express.static('public')) // Allows the use for css
+app.use(express.urlencoded({extended: true})) // Allows us to grab and use form data
+
+
 
 
 
@@ -26,8 +30,56 @@ app.use('/static', express.static('public'))
 
 
 
+
+
 /////////////////////////////////////////
 //         DATABASE CONNECTION         //
+/////////////////////////////////////////
+
+const DATABASE_URL = process.env.DATABASE_URL // Allows us to hide and use our database
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+
+// ESTABLISH OUR CONNECTION
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', ()=> console.log('Disconnected from Mongoose'))
+    .on('error', (error) => console.log(error))
+
+
+
+
+/////////////////////////////////////////
+//         FRUITS MODELS               //
+/////////////////////////////////////////
+
+const {Schema, model} = mongoose // Destructuring, grabbing model and schema off mongoose variable
+// Like saying
+// mongoose.Schema
+// mongoose.model
+
+
+// How our data will need to look when putting it into the database, it wont accept anything else unless we change it here.
+const fruitsSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+const Fruit = model('fruit', fruitsSchema)
+
+
+
+
+
+
+/////////////////////////////////////////
+//          PORT LISTENER              //
 /////////////////////////////////////////
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
